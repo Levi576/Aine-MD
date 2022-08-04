ce2d9f612bf09e8718241b463cea7ff781d556ef
Levi <110154273+Levi576@users.noreply.github.com>
2022-08-05 05:57:33 +0800

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,17 +1,20 @@
-const { tiktokdl } = require('@bochilteam/scraper')
-let handler = async (m, { conn, args, usedPrefix, command, isOwner }) => {
-	if (!args[0]) throw `Link tiktoknya mana?\n\ncontoh:\n${usedPrefix}${command} https://vm.tiktok.com/ZGJAmhSrp/`
-    tiktokdl(args[0]).then(r => {
-    let video = r.video.no_watermark
-    conn.sendFile(m.chat, video, '', `*${wm}*`, m)
-    })
+import { tiktokdl } from '@bochilteam/scraper'
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) throw `Use example ${usedPrefix}${command} https://www.tiktok.com/@omagadsus/video/7025456384175017243`
+    const { author: { nickname }, video, description } = await tiktokdl(args[0])
+    const url = video.no_watermark || video.no_watermark2 || video.no_watermark_raw
+    if (!url) throw 'Can\'t download video!'
+    conn.sendFile(m.chat, url, 'tiktok.mp4', `*TIKTOK DOWNLOADER*
+*Nickname:* ${nickname}
+*Description:* ${description}
+
+_©Sad-Bot_
+`.trim(), m)
 }
-handler.help = ['tiktok'].map(v => v + ' <url>')
+handler.help = ['tiktok', 'tiktok', 'tiktokdl'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.limit = true
-handler.group = true
+handler.command = /^(tik(tok)?(tok)?(dl)?)$/i
 
-handler.command = /^(tt|tiktok|tik)$/i
-
-module.exports = handler
+export default handler
 
